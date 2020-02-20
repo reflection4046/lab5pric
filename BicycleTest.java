27a666eba58150f385c5935a8e44c6caf629115b
Nahidul Isalm <60182620+reflection4046@users.noreply.github.com>
2020-02-20 22:59:32 +0600

    testclass

---
+++ BicycleTest.java
@@ -0,0 +1,15 @@
+package lab5.com;
+
+public class BicycleTest {
+
+	public static void main(String[] args) {
+		
+	Bicycle B1 =new Bicycle ( 12,32);
+	B1.Show();
+	B1.Apply_Break(60);
+	
+	B1.speed(45);
+	
+
+}
+}
